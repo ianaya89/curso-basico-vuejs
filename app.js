960eef5772fa3ee201ca9ba663211d7bfba32fde
Ignacio Anaya <ignacio.anaya89@gmail.com>
2019-11-12 11:46:36 -0300

    Add toggle prices and v-on

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ new Vue({
       name: 'Bitcoin',
       img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
       changePercent: 0,
-      prices: [8400, 7900, 82000, 9000, 9400, 10000, 98000],
+
       pricesWithDays: [
         { day: 'Lunes', value: 8400 },
         { day: 'Martes', value: 7900 },
@@ -16,6 +16,14 @@ new Vue({
         { day: 'Sabado', value: 10000 },
         { day: 'Domingo', value: 10200 },
       ],
+
+      showPrices: false
+    }
+  },
+
+  methods: {
+    toggleShowPrices () {
+      this.showPrices = !this.showPrices
     }
   }
 })
