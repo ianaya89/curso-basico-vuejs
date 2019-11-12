4883789bed156d4918b55811f0d3a381a710d48f
Ignacio Anaya <ignacio.anaya89@gmail.com>
2019-11-12 11:35:03 -0300

    Add flow directives

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ new Vue({
   data () {
     return {
       name: 'Bitcoin',
-      img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png'
+      img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+      changePercent: 0
     }
   }
 })
