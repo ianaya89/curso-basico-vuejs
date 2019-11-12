229f3f51094aed17f1bcfe77f758889b2f4d9804
Ignacio Anaya <ignacio.anaya89@gmail.com>
2019-11-12 12:33:49 -0300

    Emit events

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ Vue.component('CoinDetail', {
   methods: {
     toggleShowPrices () {
       this.showPrices = !this.showPrices
+
+      this.$emit('change-color', this.showPrices ? 'FF96C8' : '3D3D3D')
     }
   },
 
@@ -90,10 +92,12 @@ new Vue({
     }
   },
 
-  // methods: {
-  //   toggleShowPrices () {
-  //     this.showPrices = !this.showPrices
-  //     this.color = this.color.split('').reverse().join('')
-  //   }
-  // }
+  methods: {
+    updateColor (color) {
+      this.color = color || this.color
+        .split('')
+        .reverse()
+        .join('')
+    }
+  }
 })
